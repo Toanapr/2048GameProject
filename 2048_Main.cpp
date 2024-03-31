ee7cb7e17ecc36afbc3b37f320060a8181846321
Thai Toan <toanhuynh.apr@gmail.com>
2024-03-31 22:27:39 +0700

    complete move

--- 2048_Main.cpp
+++ 2048_Main.cpp
@@ -18,16 +18,143 @@ void random(int **a, int n)
         a[pos3][pos4] = 2;
     } while (pos1 == pos3 && pos2 == pos4);
 }
-void creatNewGame(int **&a, int &n)
+int **allocateMatrix(int n)
 {
     n = 4;
-    a = new int *[n];
+    int **a = new int *[n];
     for (int i = 0; i < n; i++)
         a[i] = new int[n];
+    return a;
+}
+void creatNewGame(int **&a, int &n)
+{
+    a = allocateMatrix(n);
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             a[i][j] = 0;
     random(a, n);
+}
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void move(int **a, int n, int index, bool isRow)
+{
+    if (!isRow)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            int temp = i + 1;
+            while (a[i][index])
+                ;
+        }
+    }
+}
+void moveLeft(int **a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int temp = j + 1;
+            while (temp < n && a[i][temp] == 0)
+            {
+                temp++;
+            }
+            if (temp >= n)
+                break;
+            if (a[i][j] == 0)
+            {
+                swap(&a[i][j], &a[i][temp]);
+                j--;
+            }
+            if (a[i][j] == a[i][temp])
+            {
+                a[i][j] *= 2;
+                a[i][temp] = 0;
+            }
+        }
+    }
+}
+void moveRight(int **a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n - 1; j >= 0; j--)
+        {
+            int temp = j - 1;
+            while (temp >= 0 && a[i][temp] == 0)
+            {
+                temp--;
+            }
+            if (temp < 0)
+                break;
+            if (a[i][j] == 0)
+            {
+                swap(&a[i][j], &a[i][temp]);
+                j++;
+            }
+            if (a[i][j] == a[i][temp])
+            {
+                a[i][j] *= 2;
+                a[i][temp] = 0;
+            }
+        }
+    }
+}
+void moveDown(int **a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n - 1; j >= 0; j--)
+        {
+            int temp = j - 1;
+            while (temp >= 0 && a[temp][i] == 0)
+            {
+                temp--;
+            }
+            if (temp < 0)
+                break;
+            if (a[j][i] == 0)
+            {
+                swap(&a[j][i], &a[temp][i]);
+                j++;
+            }
+            else if (a[j][i] == a[temp][i])
+            {
+                a[j][i] *= 2;
+                a[temp][i] = 0;
+            }
+        }
+    }
+}
+void moveUp(int **a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int temp = j + 1;
+            while (temp < n && a[temp][i] == 0)
+            {
+                temp++;
+            }
+            if (temp >= n)
+                break;
+            if (a[j][i] == 0)
+            {
+                swap(&a[j][i], &a[temp][i]);
+                j--;
+            }
+            else if (a[j][i] == a[temp][i])
+            {
+                a[j][i] *= 2;
+                a[temp][i] = 0;
+            }
+        }
+    }
 }
 void printUI(int **a, int n)
 {
@@ -43,10 +170,12 @@ void printUI(int **a, int n)
         cout << endl;
     }
 }
+
 void deletePoiter(int **&a, int n)
 {
     for (int i = 0; i < n; i++)
         delete[] a[i];
+    delete a;
 }
 int main()
 {
@@ -54,6 +183,33 @@ int main()
     int n = 4;
     srand(time(0));
     creatNewGame(a, n);
+    printUI(a, n);
+    char x;
+    while (true)
+    {
+        cin >> x;
+        if (x == 'w')
+            moveUp(a, n);
+        if (x == 's')
+            moveDown(a, n);
+        if (x == 'a')
+            moveLeft(a, n);
+        if (x == 'd')
+            moveRight(a, n);
+        if (x == 'q')
+            break;
+        int pos3 = rand() % n;
+        int pos4 = rand() % n;
+        while (a[pos3][pos4] != 0)
+        {
+            pos3 = rand() % n;
+            pos4 = rand() % n;
+        }
+        a[pos3][pos4] = 2;
+        printUI(a, n);
+        cout << endl;
+    }
+    moveRight(a, n);
     printUI(a, n);
     deletePoiter(a, n);
     return 0;
